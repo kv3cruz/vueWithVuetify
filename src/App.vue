6bfc8b70ee9ffae72496ce9a43f4b6df2a1c8964
FIRST_NAME LAST_NAME <kv3delacruz@gmail.com>
2021-08-12 13:18:19 -0600

    Adding an app var

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,40 @@
 <template>
    <v-app>
-      <v-card width="400" class="mx-auto mt-5">
-         <v-card-title>
-            <h1 class="display-1">Login</h1>
-         </v-card-title>
-         <v-card-text>
-            <v-form>
-               <v-text-field label="Username" prepend-icon="mdi-account-circle">
-               </v-text-field>
-               <v-text-field
-                  label="Password"
-                  prepend-icon="mdi-lock"
-                  :append-icon="!showPassword ? 'mdi-eye-off' : 'mdi-eye'"
-                  :type="showPassword ? 'text' : 'password'"
-                  @click:append="showPassword = !showPassword"
-               ></v-text-field>
-            </v-form>
-         </v-card-text>
-         <v-divider></v-divider>
-         <v-card-actions>
-            <v-btn color="success">Register</v-btn>
-            <v-spacer></v-spacer>
-            <v-btn color="info">Login</v-btn>
-         </v-card-actions>
-      </v-card>
+      <v-app-bar app color="primary" dark>
+         <v-toolbar-title> Vuetify Dashboard </v-toolbar-title>
+         <v-spacer></v-spacer>
+         <v-btn>Home</v-btn>
+         <v-btn>Login</v-btn>
+      </v-app-bar>
+      <v-content>
+         <v-card width="400" class="mx-auto mt-5">
+            <v-card-title>
+               <h1 class="display-1">Login</h1>
+            </v-card-title>
+            <v-card-text>
+               <v-form>
+                  <v-text-field
+                     label="Username"
+                     prepend-icon="mdi-account-circle"
+                  >
+                  </v-text-field>
+                  <v-text-field
+                     label="Password"
+                     prepend-icon="mdi-lock"
+                     :append-icon="!showPassword ? 'mdi-eye-off' : 'mdi-eye'"
+                     :type="showPassword ? 'text' : 'password'"
+                     @click:append="showPassword = !showPassword"
+                  ></v-text-field>
+               </v-form>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions>
+               <v-btn color="success">Register</v-btn>
+               <v-spacer></v-spacer>
+               <v-btn color="info">Login</v-btn>
+            </v-card-actions>
+         </v-card>
+      </v-content>
    </v-app>
 </template>
 
