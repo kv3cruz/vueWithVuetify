c4122aa771401c9ae7162620920f32c4af3e6093
FIRST_NAME LAST_NAME <kv3delacruz@gmail.com>
2021-08-13 16:30:01 -0600

    Vuetify  layouts grid system

---
+++ src/components/EmployeesTable.vue
@@ -0,0 +1,42 @@
+<template>
+   <v-data-table
+      :headers="headers"
+      :items="employees"
+      :items-per-page="5"
+      class="elevation-1"
+      :multi-sort="true"
+      @click:row="selectRow"
+   ></v-data-table>
+</template>
+
+<script>
+export default {
+   name: 'EmployeesTable',
+   props: {
+      employees: {
+         type: Array,
+         required: true
+      }
+   },
+   data: () => ({
+      headers: [
+         { text: 'Employee ID', value: 'id' },
+         { text: 'Name', value: 'name' },
+         { text: 'Position Title', value: 'title' },
+         { text: 'Salary', value: 'salary' }
+      ]
+   }),
+   methods: {
+      selectRow(event) {
+         const employee = {
+            name: event.name,
+            title: event.title
+         }
+
+         this.$emit('select-employee', employee)
+      }
+   }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,142 +1,75 @@
 <template>
-   <v-main>
-      <v-snackbar v-model="snackbar" :timeout="timeout" :color="snackColor">
-         {{ snackText }}
+   <v-container>
+      <h1>Dashboard</h1>
+      <v-row>
+         <v-col v-for="sale in sales" :key="`${sale.title}`" cols="12" md="4">
+            <SalesGraph :sale="sale" />
+         </v-col>
+      </v-row>
 
-         <template v-slot:action="{ attrs }">
-            <v-btn color="red" text v-bind="attrs" @click="snackbar = false">
-               Close
-            </v-btn>
-         </template>
+      <v-row>
+         <v-col v-for="statistic in statistics" :key="`${statistic.title}`">
+            <StatisticCard :statistic="statistic" />
+         </v-col>
+      </v-row>
+      <v-row>
+         <v-col cols="8">
+            <EmployeesTable
+               :employees="employees"
+               @select-employee="setEmployee"
+            />
+         </v-col>
+         <v-col cols="4">
+            <EventTimeline :timeline="timeline" />
+         </v-col>
+      </v-row>
+
+      <v-snackbar v-model="snackbar" :left="$vuetify.breakpoint.mdAndUp">
+         You have selected {{ selectedEmployee.name }},
+         {{ selectedEmployee.title }}
+         <v-btn color="pink" text @click="snackbar = false"> Close </v-btn>
       </v-snackbar>
-      <v-card width="800" class="mx-auto mt-5">
-         <v-card-title class="d-flex justify-center">
-            <h1>Dashboard</h1>
-         </v-card-title>
-         <v-divider></v-divider>
-         <v-data-table
-            :headers="headers"
-            :items="desserts"
-            :items-per-page="5"
-            class="elevation-1"
-            @click:row="selectedRow"
-         ></v-data-table>
-      </v-card>
-   </v-main>
+   </v-container>
 </template>
 
 <script>
+import EmployeesTable from '../components/EmployeesTable'
+import EventTimeline from '../components/EventTimeline'
+import SalesGraph from '../components/SalesGraph'
+import StatisticCard from '../components/StatisticCard'
+
+import employeesData from '../data/employees.json'
+import timelineData from '../data/timeline.json'
+import salesData from '../data/sales.json'
+import statisticsData from '../data/statistics.json'
+
 export default {
+   name: 'DashboardPage',
+   components: {
+      EmployeesTable,
+      EventTimeline,
+      SalesGraph,
+      StatisticCard
+   },
    data() {
       return {
-         headers: [
-            {
-               text: 'Dessert (100g serving)',
-               align: 'start',
-               sortable: false,
-               value: 'name'
-            },
-            { text: 'Calories', value: 'calories' },
-            { text: 'Fat (g)', value: 'fat' },
-            { text: 'Carbs (g)', value: 'carbs' },
-            { text: 'Protein (g)', value: 'protein' },
-            { text: 'Iron (%)', value: 'iron' }
-         ],
-         desserts: [
-            {
-               name: 'Frozen Yogurt',
-               calories: 159,
-               fat: 6.0,
-               carbs: 24,
-               protein: 4.0,
-               iron: '1%'
-            },
-            {
-               name: 'Ice cream sandwich',
-               calories: 237,
-               fat: 9.0,
-               carbs: 37,
-               protein: 4.3,
-               iron: '1%'
-            },
-            {
-               name: 'Eclair',
-               calories: 262,
-               fat: 16.0,
-               carbs: 23,
-               protein: 6.0,
-               iron: '7%'
-            },
-            {
-               name: 'Cupcake',
-               calories: 305,
-               fat: 3.7,
-               carbs: 67,
-               protein: 4.3,
-               iron: '8%'
-            },
-            {
-               name: 'Gingerbread',
-               calories: 356,
-               fat: 16.0,
-               carbs: 49,
-               protein: 3.9,
-               iron: '16%'
-            },
-            {
-               name: 'Jelly bean',
-               calories: 375,
-               fat: 0.0,
-               carbs: 94,
-               protein: 0.0,
-               iron: '0%'
-            },
-            {
-               name: 'Lollipop',
-               calories: 392,
-               fat: 0.2,
-               carbs: 98,
-               protein: 0,
-               iron: '2%'
-            },
-            {
-               name: 'Honeycomb',
-               calories: 408,
-               fat: 3.2,
-               carbs: 87,
-               protein: 6.5,
-               iron: '45%'
-            },
-            {
-               name: 'Donut',
-               calories: 452,
-               fat: 25.0,
-               carbs: 51,
-               protein: 4.9,
-               iron: '22%'
-            },
-            {
-               name: 'KitKat',
-               calories: 518,
-               fat: 26.0,
-               carbs: 65,
-               protein: 7,
-               iron: '6%'
-            }
-         ],
+         employees: employeesData,
+         sales: salesData,
+         selectedEmployee: {
+            name: '',
+            title: ''
+         },
          snackbar: false,
-         snackText: 'Open',
-         snackColor: 'primary',
-         timeout: 2000
+         statistics: statisticsData,
+         timeline: timelineData
       }
    },
    methods: {
-      selectedRow(event) {
+      setEmployee(event) {
          this.snackbar = true
-         this.snackText = event.name
+         this.selectedEmployee.name = event.name
+         this.selectedEmployee.title = event.title
       }
    }
 }
 </script>
-
-<style lang="scss" scoped></style>
